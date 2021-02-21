642a1c01e01324e59347576dea2537f62db160c6
stinkymonkeyph <monkeydev.team@gmail.com>
2021-02-21 19:56:20 +0800

    add password wall component

---
+++ src/components/layout/PasswordWall/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PasswordWall = ({ children }) => {
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+}
+
+export default PasswordWall
