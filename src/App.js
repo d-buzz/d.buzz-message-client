dbeb15633b7f306674e1421402bbce25b8e174e1
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2021-02-21 21:25:41 +0800

    add password wall (#4)
    * add password wall component
    
    * add conditions
    
    * use keydown
    
    * fix keycode condition

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React from "react";
 import { renderRoutes } from 'react-router-config'
 import { withRouter } from 'react-router'
 import routes from "./routes";
-import { AuthGuard, Init, ThemeLoader } from "./components"
+import { AuthGuard, Init, ThemeLoader, PasswordWall } from "./components"
 
 
 function App() {
   return (
     <ThemeLoader>
-      <Init>
-        <AuthGuard>
-          {renderRoutes(routes)}
-        </AuthGuard>
-      </Init>
+      <PasswordWall>
+        <Init>
+          <AuthGuard>
+            {renderRoutes(routes)}
+          </AuthGuard>
+        </Init>
+      </PasswordWall>
     </ThemeLoader>
   );
 }
